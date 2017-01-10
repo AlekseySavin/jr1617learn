a5c511f83ee67bcce7de3a990d2f5c5e16639ac7
xyzolo <saleksey67@gmail.com>
2017-01-10 11:01:29 +0300

    test  accepted

--- src/com/javarush/test/level07/lesson12/bonus03/Solution.java
+++ src/com/javarush/test/level07/lesson12/bonus03/Solution.java
@@ -29,5 +29,22 @@ public class Solution
     public static void sort(int[] array)
     {
         //напишите тут ваш код
+        int[] sorted = new int[array.length];
+        sorted[0] = 0;
+
+        for (int i = 0; i < array.length; i++)
+        {
+            for (int j = 0; j < sorted.length; j++)
+            {
+                if (array[i] >= sorted[i])
+                {
+                    sorted[i] = array[i];
+                }
+                if (array[i] <= sorted[i])
+                {
+                    sorted[i] = array[i];
+                }
+            }
+        }
     }
 }
