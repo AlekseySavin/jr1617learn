664991e16802496235bfbd10b80af6b3dec76fd1
xsd <saleksey67@gmail.com>
2017-01-25 18:33:12 +0300

    использовал сортировку пузырьком

--- src/com/javarush/test/level09/lesson11/bonus03/Solution.java
+++ src/com/javarush/test/level09/lesson11/bonus03/Solution.java
@@ -48,35 +48,74 @@ public class Solution {
 
     public static void sort(String[] array) {
         //напишите тут ваш код
-        ArrayList<String> clist = new ArrayList<String>();
-        ArrayList<String> cnums = new ArrayList<String>();
+        int counterW = 0;
+        int counterN = 0;
 
-
-        for (int i = 0; i < array.length; i++) {
-            if (isNumber(array[i]) == false) {
-                System.out.println("is number - false");
-                clist.add(array[i]);
+        for (int i = 0; i < array.length ; i++) {
+            if (isNumber(array[i])) {
+                counterN++;
             }
-            else {
-                System.out.println("is number - true");
-                for (int j = 0; j < array.length; j++) {
-                    if(isGreaterThan(array[j], array[i]) == true) {
-                        cnums.add(0, array[i]);
-                    }
-                    else {
-                        cnums.add(j,array[i]);
-                    }
+            else
+                counterW++;
+        }
+
+        String words[] = new String[counterW];
+        String numbers[] = new String[counterN];
+
+        counterW=0;
+        counterN=0;
+
+        for (int i = 0; i < array.length ; i++) {
+            if (isNumber(array[i])) {
+                numbers[counterN] = array[i];
+                counterN++;
+            } else {
+                words[counterW] = array[i];
+                counterW++;
+            }
+        }
+
+         //desc bubble sort
+
+        String temp;
+        for (int i=0;i<numbers.length;i++) {
+            for (int j=1;j<numbers.length-i;j++) {
+                int a = Integer.parseInt(numbers[j-1]);
+                int b = Integer.parseInt(numbers[j]);
+                if(a<b) {
+                    temp =numbers[j-1];
+                    numbers[j-1]=numbers[j];
+                    numbers[j]=temp;
                 }
             }
         }
 
-        for (String nums: cnums
-             ) {
-            System.out.println(nums);
+        //asc bubble sort
 
+        for (int i = 0; i < words.length ; i++) {
+            for (int j = 0; j < words.length-1 ; j++) {
+                if (isGreaterThan(words[j], words[j+1])) {
+                    temp = words[j];
+                    words [j] = words [j+1];
+                    words [j+1] = temp;
+                }
+            }
+            
         }
 
+        counterW = 0;
+        counterN = 0;
 
+        for (int i = 0; i < array.length ; i++) {
+            if (isNumber(array[i])) {
+                array[i] = numbers[counterN];
+                counterN++;
+            }
+            else {
+                array[i] = words[counterW];
+                counterW++;
+            }
+        }
     }
 
     //Метод для сравнения строк: 'а' больше чем 'b'
